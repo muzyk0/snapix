0f6d6a10cba587d3ed3d43a8c194c4f531147d73
Музыка Владислав <ru9art@gmail.com>
2024-01-05 15:56:06 +0300

    fix: Fix config service test

--- libs/config/src/config.service.spec.ts
+++ libs/config/src/config.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, type TestingModule } from '@nestjs/testing'
 import { AppConfigService } from './app-config.service'
+import { ConfigService } from '@nestjs/config'
 
 describe('ConfigService', () => {
   let service: AppConfigService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AppConfigService],
+      providers: [AppConfigService, ConfigService],
     }).compile()
 
     service = module.get<AppConfigService>(AppConfigService)
