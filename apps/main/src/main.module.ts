9df304c51e0c1c12fa14a86384fa76c96ec2840e
Музыка Владислав <ru9art@gmail.com>
2024-01-05 14:54:37 +0300

    feat: Add config service

--- apps/main/src/main.module.ts
+++ apps/main/src/main.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common'
 import { MainController } from './main.controller'
 import { MainService } from './main.service'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [MainController],
   providers: [MainService],
 })
--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { MainModule } from './main.module'
+import { ConfigService } from '@nestjs/config'
+import { Logger } from '@nestjs/common'
 
 const PORT = 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule)
-  await app.listen(PORT)
+
+  const configService = app.get<ConfigService>(ConfigService)
+
+  await app.listen(configService.get('PORT', { infer: true }) ?? PORT)
+  Logger.log(`Application is running on: ${await app.getUrl()}`)
 }
+
 void bootstrap()
