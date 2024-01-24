e747c51f4efd49972c962abbd4c27cb987e990fd
KamenshikBackTeam <155596415+KamenshikBackTeam@users.noreply.github.com>
2024-01-24 21:37:53 +0300

    build(env): shifted env variables to github secrets (#10)
    * build(env): shifted env variables to github secrets
    
    * fix github actions
    
    * override prisma datasourseUrl
    
    ---------
    
    Co-authored-by: YurL0014 <ylogachev-sfedu@mail.ru>
    Co-authored-by: Музыка Владислав <ru9art@gmail.com>

--- libs/prisma/src/prisma.service.ts
+++ libs/prisma/src/prisma.service.ts
@@ -1,8 +1,15 @@
 import { Injectable, type OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
+import { AppConfigService } from '@app/config'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(appConfigService: AppConfigService) {
+    super({
+      datasourceUrl: appConfigService.datasourceUrl,
+    })
+  }
+
   async onModuleInit() {
     // Note: this is optional
     await this.$connect()
