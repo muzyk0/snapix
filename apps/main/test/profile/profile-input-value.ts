bf4d58fabc101b99159e33070a12cab6999c6141
Julian871 <vindiesel.1999@mail.ru>
2024-03-02 14:22:51 +0300

    feat: add tests to profile fill out

---
+++ apps/main/test/profile/profile-input-value.ts
@@ -0,0 +1,19 @@
+    // correct input values
+export const username = 'newUser1'
+export const email = 'newUser1@example.com'
+export const password = 'password0aA!='
+export const userFirstName = 'Abraham'
+export const userLastName = 'Lincoln'
+export const birthDate = '12.02.1809'
+export const city = 'Kentucky'
+export const aboutMe = 'Abraham Lincoln was the 16th President of the United States, serving from 1861 until his assassination in 1865.'
+
+    // incorrect input values (inc)
+export const incUserName = 'Abrah'
+export const incUserFirstName = 1234
+export const incUserLastName = 'Lin_Coln'
+export const incBirthDate = '12 февраль 1809'
+export const incCity = 'Kentucky'
+export const incAboutMe = 'Okey, this text more then 200 symbols:' +
+    'The quick brown fox jumps over the lazy dog. Pack my box with five dozen liquor jugs.' +
+    'Amazingly few discotheques provide jukeboxes. Frequently astounding zebra playing jazz.'
