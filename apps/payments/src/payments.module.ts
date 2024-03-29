c1b80fd7a1f7a15ff55dcdbdc47835c185615264
Музыка Владислав <ru9art@gmail.com>
2024-03-29 10:17:02 +0300

    feat: fix run payments microservice

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common'
 import { PaymentsController } from './payments.controller'
 import { PaymentsService } from './payments.service'
 import { HealthModule } from '@app/core/health/health.module'
+import { AppConfigModule } from '@app/config'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.test'],
+    }),
+    AppConfigModule,
+    HealthModule,
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
