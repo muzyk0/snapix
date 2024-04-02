adf0a2b8203e8e5dc12ff5d0cad66bd2b175b35e
Музыка Владислав <ru9art@gmail.com>
2024-04-02 22:49:02 +0300

    Change storage service port

--- apps/storage/src/main.ts
+++ apps/storage/src/main.ts
@@ -1,18 +1,31 @@
 import { NestFactory } from '@nestjs/core'
 import { StorageModule } from './storage.module'
 import { type MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { type INestApplication, Logger } from '@nestjs/common'
+import type { Express } from 'express'
+import { AppConfigService } from '@app/config'
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(StorageModule, {
+  const logger = new Logger('NestBootstrap Storage')
+  const app = await NestFactory.create<INestApplication<Express>>(StorageModule)
+
+  await app.init()
+
+  const appConfigService = app.get<AppConfigService>(AppConfigService)
+
+  const host = appConfigService.storageService.host || '0.0.0.0'
+
+  app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
-      host: process.env.STORAGE_SERVICE_HOST ?? '0.0.0.0',
-      port: Number(process.env.STORAGE_SERVICE_PORT) || 3247,
+      host,
+      port: appConfigService.storageService.port,
     },
   })
 
-  console.log(process.env.STORAGE_SERVICE_PORT)
-  await app.listen()
+  await app.startAllMicroservices()
+  logger.log('Microservice Notifier is running')
+  logger.log(`host: ${host}:${appConfigService.storageService.port}`)
 }
 
 void bootstrap()
